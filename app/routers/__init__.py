75fbd076188b01ba5ddbb55d27d0ec20449ff0a4
Ekapong Kongwirut <ekapong@belusys.com>
2025-05-19 02:26:54 +0700

    quick update

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
-from app.routers import entity, import_router, user
+from app.routers import user
 
 # สร้าง APIRouter หลัก
 router = APIRouter()
 
 # รวม router ย่อยเข้าด้วยกัน
-router.include_router(entity.router)
-router.include_router(import_router.router)
 router.include_router(user.router)
 
